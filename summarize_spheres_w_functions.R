aefcdb7abc2a80349dc4fe8b48fb9c19f1a2ca91
Luis Andres Guillen <luisguialm@gmail.com>
2021-04-29 11:02:41 +0200

    commented on summary code

--- summarize_spheres_w_functions.R
+++ summarize_spheres_w_functions.R
@@ -24,26 +24,28 @@ library(sf)
 
 #Declaring variables ----
 #Read in normalized LAS data
-
 tlsData <- readLAS("../FS_fuel_prediction/raw_data/Compartment10_laz_files/C10_Plot_10East.laz")
 tlsData<-clip_circle(tlsData,0,0,10)
+
 #Define radius of sphere
 radii <- c(1, 2, 3, 4, 5)#, 6, 7, 8, 9, 10) 
 
 #Declaring constants ----
-height = 2
-thetaDiv=.5
-phiDiv=.5
-grdDist=.25
+height = 2 #height of the scanner 
+thetaDiv=.5 #
+phiDiv=.5 #
+grdDist=.25 #
 r_blank <- raster(ncol= 360/thetaDiv, nrow = 180/phiDiv, 
-                  xmn=-180, xmx=180,ymn=-90,ymx=90)
+                  xmn=-180, xmx=180,ymn=-90,ymx=90) # blank raster to fill in data
 
 # Declaring functions ----
 
 cld_fun<-function(r,tlsData){
-  #Aim:Calculate coordinates where each ray would intersect the sphere
+  #Aim: Calculate coordinates where each ray would intersect the sphere
   #Arguments:r = radius; tlsData: LAS object
   #Output: cld dataframe. 
+  
+  # create a dataframe to calculate distances to the scanner
   cld <- as.data.frame(cbind(x=tlsData@data$X, y=tlsData@data$Y, z=tlsData@data$Z - height))
   cld$a <- cld$x^2 + cld$y^2 + cld$z^2
   cld$t1 <- -sqrt(0^2-(4*cld$a*(-r^2)))/(2*cld$a)
@@ -67,7 +69,7 @@ cld_fun<-function(r,tlsData){
 
 sphere_func<-function(cld=cld){
   #Aim: calculate spherical coordinates. 
-  #Arguments: cld = dataframe with cartesian distances. 
+  #Arguments: cld: dataframe with cartesian distances obtained with cld_fun
   #Output: list of objects s3 and p3. 
   
   #Filter points that would hit the ground before hitting the sphere
@@ -96,7 +98,6 @@ sphere_func<-function(cld=cld){
   #Create table of x, y, z values where each point that passed the ground intersected it
   passF <- bind_rows(pass_sphere, pass_sphere_g)
   
-  
   #Convert from Cartesian to spherical coordinates
   s <- as.data.frame(cart2sph(x=allF$xs, 
                               y=allF$ys, 
@@ -113,16 +114,20 @@ sphere_func<-function(cld=cld){
   p2$theta <- p$theta*(180/pi)
   p2$phi <- p$phi*(180/pi)
   
-  s3 <- st_multipoint(x=as.matrix(s2), dim="XY")
-  p3 <- st_multipoint(x=as.matrix(p2), dim="XY")
+  s3 <- st_multipoint(x=as.matrix(s2), dim="XY") # make spatial object
+  p3 <- st_multipoint(x=as.matrix(p2), dim="XY") # make spatial object
   
-  multipoint_list<-list(s3,p3) 
+  #create lists and finalize function
+  multipoint_list<-list(s3,p3)
   names(multipoint_list)<-c("s3","p3")
   return(multipoint_list)
 } 
 
 pointCount <- function(r, pts){
-  #From: https://gis.stackexchange.com/questions/309407/computing-number-of-points-in-a-raster-grid-cell-in-r
+  # Idea source: https://gis.stackexchange.com/questions/309407/computing-number-of-points-in-a-raster-grid-cell-in-r
+  # Aim: Calculate the number of points in a raster cell grid. 
+  # Argument: r: radious
+  # Output: r2: raster object with points. 
   
   # make a raster of zeroes like the input
   r2 = r
@@ -135,55 +140,60 @@ pointCount <- function(r, pts){
 }
 
 on_sphere_multi_v2 <- function(tlsData, radii, height, thetaDiv=1, phiDiv=1){
-  for(ra in radii){
-    if(ra == radii[1]){
-      r <- ra
-      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func()
+  # Aim: Transforms a point cloud into a raster showing the points that passed or not pass a certain radious, 
+  # depending on the layer of the raster.
+  # Arguments: tlsData: cloudpoint, radii: list of radious, height: height of scanner.
+  # Output: raster object with different layers, representing each radious. 
+  for(ra in radii){ # Begin for radious loop 
+    if(ra == radii[1]){ # Logic statement of 1st radius
+      r <- ra #read radius
+      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func() # apply functions to obtain spherical coordinates 
       
-      gridS <- pointCount(r_blank,list_point$s3)
-      gridP <- pointCount(r_blank, list_point$p3)
+      gridS <- pointCount(r_blank,list_point$s3) #create point object of points on the sphere
+      gridP <- pointCount(r_blank, list_point$p3)#create point object of points that pass the sphere
 
-      null1 <- raster::calc(gridS, function(x){x[x<=0]<-NA; return(x)})
-      null2 <- null1 > 0
+      null1 <- raster::calc(gridS, function(x){x[x<=0]<-NA; return(x)}) # make raster??
+      null2 <- null1 > 0 #??
 
-      out <- (1-(gridP/gridS))*null2
-    }else if(ra == radii[2]){
-      r<-ra
-      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func()
+      out <- (1-(gridP/gridS))*null2 #create raster for first radius
+    }else if(ra == radii[2]){# logical statement 2nd radius
+      r<-ra # read radius
+      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func() # apply functions to obtain spherical coordinates 
       
-      gridPre <- gridS - gridP
+      gridPre <- gridS - gridP # make temporal raster of difference. 
     
-      gridS <- pointCount(r_blank, list_point$s3) - gridPre
-      gridP <- pointCount(r_blank, list_point$p3) 
+      gridS <- pointCount(r_blank, list_point$s3) - gridPre # create point object of points on the sphere minus the previous sphere
+      gridP <- pointCount(r_blank, list_point$p3) #create point object of points that pass the sphere
     
-      mask1 <- out
-      mask2 <- raster::calc(mask1, function(x){x[x==1]<-NA; return(x)})
-      mask3 <- mask2 >= 0
+      mask1 <- out # read previous layer. 
+      mask2 <- raster::calc(mask1, function(x){x[x==1]<-NA; return(x)}) #??
+      mask3 <- mask2 >= 0 #??
     
-      out <- (1-(gridP/gridS))
-      out2 <- stack(mask1, out)
-    }else{
-      r<-ra
-      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func()
-      gridPre <- gridS - gridP
+      out <- (1-(gridP/gridS)) # make faster for 2nd radius
+      out2 <- stack(mask1, out) # stack rasters
+    }else{ # logic stament n_th radius
+      r<-ra # read data
+      list_point<-cld_fun(r=r,tlsData = tlsData) %>% sphere_func() # apply functions to obtain spherical coordinates
       
-      gridS <- pointCount(r_blank, list_point$s3) - gridPre
-      gridP <- pointCount(r_blank, list_point$p3) 
+      gridPre <- gridS - gridP# make temporal raster of difference.
       
-      mask1 <- out
-      mask2 <- raster::calc(mask1, function(x){x[x==1]<-NA; return(x)})
-      mask3 <- mask2 >= 0
+      gridS <- pointCount(r_blank, list_point$s3) - gridPre # create point object of points on the sphere minus the previous sphere
+      gridP <- pointCount(r_blank, list_point$p3) #create point object of points that pass the sphere
       
-      out <- (1-(gridP/gridS))*mask3
-      out2 <- stack(out2, out)
+      mask1 <- out # read raster of previous radius.
+      mask2 <- raster::calc(mask1, function(x){x[x==1]<-NA; return(x)}) #??
+      mask3 <- mask2 >= 0 #??
+      
+      out <- (1-(gridP/gridS))*mask3 # create raster of n radius. 
+      out2 <- stack(out2, out) # stack rasters of n radius 
     }
-  }
-  names(out2) <- as.character(radii)
-  return(out2)
-}
+  }#Close radius loop
+  names(out2) <- as.character(radii)# name layers
+  return(out2) # set function output. 
+} 
 
 # Applying functions ----
 
 test <- on_sphere_multi_v2(tlsData=tlsData, radii=radii, height=height, thetaDiv=thetaDiv, phiDiv=phiDiv)
 
-writeRaster(test, "D:/sphere_vox.tif")
+writeRaster(test, "D:/sphere_vox.tif") # save data. 
